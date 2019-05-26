981d8214dc54fd5c02e44fca068ff2159d1fc286
安永拓矢 <>
2019-05-26 23:20:34 +0900

    投稿、編集、削除、ページネーション、コメント、view統一、 ログイン機能と掲示板機能ルート連携

--- sample/routes/web.php
+++ sample/routes/web.php
@@ -11,12 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('new', 'PostsController@create');
+Route::get('/', 'PostsController@index')->name('top');
+
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store','show','edit','update','destroy']]);
+
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
