30e518034cf44f070aef858afb19a9da260e4a1d
安永拓矢 <>
2019-05-26 23:20:47 +0900

    no message

---
+++ sample/database/factories/CommentFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+      'title' => '投稿のタイトル',
+     'body' => "本文です。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。\nテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト。",
+    ];
+});
